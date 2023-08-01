231b94a07636169d31ea22a73bdd88d8df7f7482
HoangLy <hoanglyquoc@gmail.com>
2023-08-01 22:32:28 +0700

    basic home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Nav, NavLink, Bars, NavMenu } from "./components/NavbarElement";
+import { BrowserRouter as Router, Routes, Route }
+    from 'react-router-dom';
+import Home from './pages/home'
+import Education from './pages/education';
+import Experience from './pages/experience';
+import Projects from './pages/projects';
+import Contact from './pages/contact';
+import NavBar from './components';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <NavBar>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/education' element={<Education />} />
+            <Route path='/experience' element={<Experience />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </NavBar>
+      </Router>
   );
 }
 
-export default App;
---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact Me</h1>
+            <ul>
+                <li>Personal Email: hoanglyquoc@gmail.com</li>
+                <li>Work Email: hoangly@umass.edu</li>
+                <li>Phone Number: 207-307-1607</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Contact;
