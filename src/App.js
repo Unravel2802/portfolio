4608de42f341e7cd3e1743603b53747f37b224ec
HoangLy <hoanglyquoc@gmail.com>
2023-08-02 07:05:26 +0700

    fix routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import './App.css';
 import React from 'react';
-import { Nav, NavLink, Bars, NavMenu } from "./components/NavbarElement";
+import './App.css';
+import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route }
-    from 'react-router-dom';
-import Home from './pages/home'
+	from 'react-router-dom';
+import Home from './pages/home';
 import Education from './pages/education';
 import Experience from './pages/experience';
 import Projects from './pages/projects';
 import Contact from './pages/contact';
-import NavBar from './components';
 
-export default function App() {
-  return (
-      <Router>
-        <NavBar>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/education' element={<Education />} />
-            <Route path='/experience' element={<Experience />} />
-            <Route path='/projects' element={<Projects />} />
-            <Route path='/contact' element={<Contact />} />
-          </Routes>
-        </NavBar>
-      </Router>
-  );
+function App() {
+	return (
+    <div>
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route exact path='/'  element={<Home />} />
+				<Route path='/experience' element={<Experience />} />
+				<Route path='/contact' element={<Contact />} />
+				<Route path='/education' element={<Education />} />
+				<Route path='/projects' element={<Projects />} />
+			</Routes>
+		</Router>
+    </div>
+	);
 }
 
+export default App;
