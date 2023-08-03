1729bb0e23c25c2e5be8ea76566b855fcb3fe5e8
HoangLy <hoanglyquoc@gmail.com>
2023-08-03 19:18:40 +0700

    home basic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Home from './components/home';
+import Contact from './components/contact';
 
 function App(){
 	return (
 		<div>
 			<Navbar />
+			<Home />
+			<Contact />
 		</div>  
 	)
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { Element } from 'react-scroll'
+import './home.css'
+import hoang from './images/about.jpg'
 
 const Home = () => {
     return (
-        <div className="page">
-			<Element name='Home Page'>
-            <h1>Home Page</h1>
-            </Element>
+        <div className="home">
+            <div className="container">
+                <img src={hoang} alt='john'/>
+                <div className="columns-2">
+                    <h2>Home</h2>
+                    <span className="line"></span>
+                    <p>CS student passionate about AI, Machine Learning, and web/app development. Explore my tech journey and innovative projects in these fields.</p>
+                    <button className="button">explore more</button>
+                </div>
+            </div>
         </div>
     )
 }
