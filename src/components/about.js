18b1a17cc332737ea30d59e232ae82218a5bf6c5
HoangLy <hoanglyquoc@gmail.com>
2023-08-04 19:51:17 +0700

    finish basic information

--- src/components/about.js
+++ src/components/about.js
@@ -1,15 +1,15 @@
 import React from "react";
 import './about.css'
-import hoang from './images/about.jpg'
 
 const About = () => {
     return (
         <div className="about">
-            <div className="container">
-                {/* <img src={hoang} alt='john'/> */}
-                <h1> Hoang Ly </h1>
-                    <p>CS student passionate about AI, Machine Learning, and web/app development. Explore my tech journey and innovative projects in these fields.</p>
-            </div>
+                <p style={{textAlign: "center"}}>Hoang Ly</p>
+                <p>Undergrad student at Umass Amherst</p>
+                <p>Interested in artificial intelligence and software engineering.</p>
+                <div className='nav'>
+
+                </div>
         </div>
     )
 }
