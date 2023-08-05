665e5f510142633fb65c5dbfaf762434214708db
HoangLy <hoanglyquoc@gmail.com>
2023-08-06 00:47:38 +0700

    finish education component

--- src/components/education.js
+++ src/components/education.js
@@ -1,9 +1,23 @@
 import React from "react";
-
+import './education.css';
 const Education = () => {
     return (
         <div className="edu">
-            Education
+            <h1>Intro</h1>
+            <p>
+                Hi, my name is Hoang Quoc Ly. I'm a sophomore studying 
+                computer science and data science at University of 
+                Masachusetts at Amherst. 
+                <br/><br/>
+                My main interest is about artificial intelligence and its branches 
+                (machine learning, deep learning, etc.). I want to 
+                especially learn more about the use of artificial intelligence in 
+                medical fields.
+                <br/><br/>
+                At the moment, I'm taking a break from artificial intelligence 
+                and learning more about web/app development. This web portfolio
+                 is my first project using react.
+            </p>
         </div>
     )
 }
