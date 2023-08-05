9afd60211a2cfdefe448006fcd8909daa184c56d
HoangLy <hoanglyquoc@gmail.com>
2023-08-05 16:30:24 +0700

    finish about

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,9 @@
 import React from "react";
-import './about.css'
+import './about.css';
+import fb from '../images/facebook.png';
+import ig from '../images/instagram.png';
+import gh from '../images/github-sign.png';
+import ld from '../images/linkedin.png';
 
 const About = () => {
     return (
@@ -8,9 +12,26 @@ const About = () => {
                 <p>Undergrad student at Umass Amherst</p>
                 <p>Interested in artificial intelligence and software engineering.</p>
                 <div className='nav'>
-                        <button className="button"> About</button>
-                        <button className="button"> Experience</button>
-                        <button className="button"> Projects</button>
+                    <button className="button"> About</button>
+                    <button className="button"> Experience</button>
+                    <button className="button"> Projects</button>
+                </div>
+                <div className="social-icons">
+                    <a href='/' className="facebook">
+                        <img src={fb} alt='facebook'/>
+                    </a>
+
+                    <a href='/' className="instagram">
+                        <img src={ig} alt='instagram'/>
+                    </a>
+
+                    <a href='/' className="linkedin">
+                        <img src={ld} alt='linkedin'/>
+                    </a>
+
+                    <a href='/' className="github">
+                        <img src={gh} alt='github'/>
+                    </a>
                 </div>
         </div>
     )
