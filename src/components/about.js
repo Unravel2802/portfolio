97d79136d3dfe49716ad0d3b8fdfaed259d9a4ed
HoangLy <hoanglyquoc@gmail.com>
2023-08-06 00:48:24 +0700

    add my image to about

--- src/components/about.js
+++ src/components/about.js
@@ -4,11 +4,12 @@ import fb from '../images/facebook.png';
 import ig from '../images/instagram.png';
 import gh from '../images/github-sign.png';
 import ld from '../images/linkedin.png';
+import hoang from '../images/about.jpg'
 
 const About = () => {
     return (
         <div className="about">
-                <p style={{textAlign: "center"}}>Hoang Ly</p>
+                <p>Hoang Ly</p>
                 <p>Undergrad student at Umass Amherst</p>
                 <p>Interested in artificial intelligence and software engineering.</p>
                     <div className="button-container">
@@ -16,23 +17,27 @@ const About = () => {
                         <button className="button"> <span>Experience</span></button>
                         <button className="button"> <span>Projects</span></button>
                     </div>
-                <div className="social-icons">
-                    <a href='/' className="facebook">
-                        <img src={fb} alt='facebook'/>
-                    </a>
-
-                    <a href='/' className="instagram">
-                        <img src={ig} alt='instagram'/>
-                    </a>
-
-                    <a href='/' className="linkedin">
-                        <img src={ld} alt='linkedin'/>
-                    </a>
-
                     <a href='/' className="github">
-                        <img src={gh} alt='github'/>
+                        <img src={hoang} alt='github'/>
                     </a>
-                </div>
+                    {/* <h2>~Puerto Rico, 2023~</h2>     */}
+                    <div className="social-icons">
+                        <a href='/' className="facebook">
+                            <img src={fb} alt='facebook'/>
+                        </a>
+
+                        <a href='/' className="instagram">
+                            <img src={ig} alt='instagram'/>
+                        </a>
+
+                        <a href='/' className="linkedin">
+                            <img src={ld} alt='linkedin'/>
+                        </a>
+
+                        <a href='/' className="github">
+                            <img src={gh} alt='github'/>
+                        </a>
+                    </div>
         </div>
     )
 }
